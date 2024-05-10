42ca39e26d916e413f5c26bd61c6345ba56bec2b
Delgorithm <delgorithm@gmail.com>
2024-05-10 17:37:47 +0200

    Feature : New visual informations (styling) whenver you go on a page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,34 +2,54 @@ import { RiHome2Line } from "react-icons/ri";
 import { IoCalendarNumberOutline } from "react-icons/io5";
 import { GoGraph } from "react-icons/go";
 import { IoPersonOutline } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+	const location = useLocation();
+
 	return (
-		<>
-			<nav className="flex justify-around fixed bottom-0 w-full py-1 border-t border-t-0.5 border-black">
-				<Link to="/" className="flex flex-col items-center rounded-lg p-1">
-					<RiHome2Line className="text-2xl" />
-					Accueil
-				</Link>
-				<Link
-					to="calendar"
-					className="flex flex-col items-center rounded-lg p-1">
-					<IoCalendarNumberOutline className="text-2xl" />
-					Calendrier
-				</Link>
-				<Link
-					to="results"
-					className="flex flex-col items-center rounded-lg p-1">
-					<GoGraph className="text-2xl" />
-					Résultats
-				</Link>
-				<Link to="profil" className="flex flex-col items-center rounded-lg p-1">
-					<IoPersonOutline className="text-2xl" />
-					Profil
-				</Link>
-			</nav>
-		</>
+		<nav className="flex justify-around fixed bottom-0 w-full py-1 border-t border-t-0.5 border-blue-200 bg-blue-100">
+			<Link
+				to="/"
+				className={
+					location.pathname === "/"
+						? "flex flex-col items-center rounded-lg p-1 bg-blue-500 text-white"
+						: "flex flex-col items-center rounded-lg p-1"
+				}>
+				<RiHome2Line className="text-2xl" />
+				Accueil
+			</Link>
+			<Link
+				to="calendar"
+				className={
+					location.pathname === "/calendar"
+						? "flex flex-col items-center rounded-lg p-1 bg-blue-500 text-white"
+						: "flex flex-col items-center rounded-lg p-1"
+				}>
+				<IoCalendarNumberOutline className="text-2xl" />
+				Calendrier
+			</Link>
+			<Link
+				to="results"
+				className={
+					location.pathname === "/results"
+						? "flex flex-col items-center rounded-lg p-1 bg-blue-500 text-white"
+						: "flex flex-col items-center rounded-lg p-1"
+				}>
+				<GoGraph className="text-2xl" />
+				Résultats
+			</Link>
+			<Link
+				to="profil"
+				className={
+					location.pathname === "/profil"
+						? "flex flex-col items-center rounded-lg p-1 bg-blue-500 text-white"
+						: "flex flex-col items-center rounded-lg p-1"
+				}>
+				<IoPersonOutline className="text-2xl" />
+				Profil
+			</Link>
+		</nav>
 	);
 }
 
--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -13,7 +13,7 @@ function Notes() {
 				className="block w-full rounded-md border-0 py-2 p-1.5 text-gray-950 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 resize-none"
 				placeholder="Partager mon ressentis"></textarea>
 			<button
-				type="button"
+				type="submit"
 				className="bg-blue-500 text-white p-2 mt-2 w-full rounded-lg">
 				Envoyer
 			</button>
