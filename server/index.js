32fd40be76ca1e291fc0692b3c25b59fb2788548
Delgorithm <delgorithm@gmail.com>
2024-05-11 13:04:41 +0200

    Feature : Added the 'GET' to select my values from the db

--- server/index.js
+++ server/index.js
@@ -24,15 +24,30 @@ connection.connect((err) => {
 	}
 });
 
+// Pour ajouter une valeur dans la db
+
 app.post("/", (req, res) => {
 	const { value, day } = req.body;
 	const sql = "INSERT INTO mesure (valeur, jour) VALUES (?, ?)";
 	connection.query(sql, [value, day], (err, result) => {
 		if (err) {
-			console.error("Errur : dans l'instertion des données : ", err);
+			console.error("Erreur : dans l'instertion des données : ", err);
 		} else {
 			console.log("Réussite : Insertion des données ");
 		}
+	});
+});
+
+// Pour récupérer la valeur dans la db
+
+app.get("/data", (req, res) => {
+	connection.query("SELECT jour, valeur FROM mesure", (err, results) => {
+		if (err) {
+			console.error("Erreur lors de la récupération des données: ", err);
+			return;
+		}
+		res.json(results);
+		console.log(results);
 	});
 });
 
