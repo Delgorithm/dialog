98da69b536b9a5d168963cd0d8b0b940503a10b1
Delgorithm <delgorithm@gmail.com>
2024-05-20 19:02:54 +0200

    Server + DB = Connected

---
+++ server/server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Configuration pour se connecter à la BDD de MySQL
+const db = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
+	port: process.env.DB_PORT,
+});
+
+db.connect((err) => {
+	if (err) {
+		console.error("Erreur de connexion à la base de données : ", err);
+	}
+	console.log("Connecté à la base de données MySQL");
+});
+
+app.get("/rates", (req, res) => {
+	const day = req.query.day;
+	console.log(day);
+	if (!day) {
+		return res.status(400).send("Le paramètre 'day' est requis");
+	}
+
+	const query = "SELECT rate FROM dialogbdd WHERE day = ?";
+	db.query(query, [day], (err, results) => {
+		if (err) {
+			console.error("Erreur lors de l'exécutioin de la requête:", err);
+			return res.status(500).send("Erreur interne du serveur");
+		}
+		res.json(results);
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Dialog app is listening on port ${port}`);
+});
