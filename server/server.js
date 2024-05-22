0d1945d0960c3af5e1f54631d779d55a3bc7df71
Delgorithm <delgorithm@gmail.com>
2024-05-22 15:25:52 +0200

    trying to add a structure in the backend

--- server/server.js
+++ server/server.js
@@ -1,44 +1,46 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql2");
-const app = express();
-const port = 3000;
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const mysql = require("mysql2");
+// const app = express();
+// const port = 3000;
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	port: process.env.DB_PORT,
-});
+// const db = mysql.createConnection({
+// 	host: process.env.DB_HOST,
+// 	user: process.env.DB_USER,
+// 	password: process.env.DB_PASSWORD,
+// 	database: process.env.DB_DATABASE,
+// 	port: process.env.DB_PORT,
+// });
 
-db.connect((err) => {
-	if (err) {
-		console.error("Erreur de connexion à la base de données : ", err);
-	}
-	console.log("Connecté à la base de données MySQL");
-});
+// db.connect((err) => {
+// 	if (err) {
+// 		console.error("Erreur de connexion à la base de données : ", err);
+// 	}
+// 	console.log("Connecté à la base de données MySQL");
+// });
 
-app.get("/rates", (req, res) => {
-	const day = req.query.day;
-	if (!day) {
-		return res.status(400).send("Le paramètre 'day' est requis");
-	}
+// app.get("/rates", (req, res) => {
+// 	const day = req.query.day;
+// 	if (!day) {
+// 		return res.status(400).send("Le paramètre 'day' est requis");
+// 	}
 
-	const query = "SELECT rate, hour FROM dialogbdd WHERE day = ?";
-	db.query(query, [day], (err, results) => {
-		if (err) {
-			console.error("Erreur lors de l'exécutioin de la requête:", err);
-			return res.status(500).send("Erreur interne du serveur");
-		}
-		res.json(results);
-	});
-});
+// 	const query = "SELECT rate, hour FROM dialogbdd WHERE day = ?";
+// 	db.query(query, [day], (err, results) => {
+// 		if (err) {
+// 			console.error("Erreur lors de l'exécutioin de la requête:", err);
+// 			return res.status(500).send("Erreur interne du serveur");
+// 		}
+// 		res.json(results);
+// 	});
+// });
 
-app.listen(port, () => {
-	console.log(`Dialog app is listening on port ${port}`);
-});
+// app.listen(port, () => {
+// 	console.log(`Dialog app is listening on port ${port}`);
+// });
+
+
