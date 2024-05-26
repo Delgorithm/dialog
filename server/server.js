bac3236316220d4f66c46a5a33f417be59aac43b
Delgorithm <delgorithm@gmail.com>
2024-05-26 12:54:50 +0200

    Feature : Datas can be send to the DB

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ db.connect((err) => {
 	console.log("Connecté à la base de données MySQL");
 });
 
-// BDD
+// BDD : To retrive the good datas from it
 app.get("/rates", (req, res) => {
 	const day = req.query.day;
 	if (!day) {
@@ -60,6 +60,22 @@ app.get("/rates", (req, res) => {
 			return res.status(500).send("Erreur interne du serveur");
 		}
 		res.json(results);
+	});
+});
+
+// BDD : To add the datas from inputs to it
+app.post("/addData", (req, res) => {
+	const data = req.body;
+
+	const query = "INSERT INTO dialogbdd (rate, hour, day) VALUES ?";
+	const values = data.map((item) => [item.rate, item.hour, item.day]);
+
+	db.query(query, [values], (err, result) => {
+		if (err) {
+			console.error("Erreur lors de l'ajout des données: ", err);
+			return res.status(500).send("Erruer interne du serveur");
+		}
+		res.json({ message: "Données ajoutées avec succès", result });
 	});
 });
 
