3173fb4180d639d79f3fd63ccf050475b0f3c077
Delgorithm <delgorithm@gmail.com>
2024-07-31 11:59:10 +0200

    Component

--- client/src/components/NavbarTemporis.jsx
+++ client/src/components/NavbarTemporis.jsx
@@ -1,21 +1,36 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthProvider";
 
-function NavbarTemporis() {
+function Navbar() {
+  const { user, logout, error } = useAuth();
+
   return (
     <nav>
       <ul className="flex gap-5 bg-blue-400 p-5">
         <li>
           <Link to="/">Accueil</Link>
         </li>
-        <li>
-          <Link to="/register">Inscription</Link>
-        </li>
-        <li>
-          <Link to="/login">Se connecter</Link>
-        </li>
+        {user ? (
+          <>
+            <p>Welcome, {user.username}</p>
+            <button type="button" onClick={logout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/register">Inscription</Link>
+            </li>
+            <li>
+              <Link to="/login">Se connecter</Link>
+            </li>
+          </>
+        )}
       </ul>
+      {error && <p>Error: {error}</p>}
     </nav>
   );
 }
 
-export default NavbarTemporis;
+export default Navbar;
